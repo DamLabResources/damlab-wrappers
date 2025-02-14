074ace54c1c9649dda264c965583fd348130607d
Will Dampier <wnd22@drexel.edu>
2025-02-14 14:25:00 -0500

    strainline plugin now makes reports

--- multiqc_plugins/damlabqc/modules/strainline/strainline.py
+++ multiqc_plugins/damlabqc/modules/strainline/strainline.py
@@ -1,18 +1,17 @@
+from collections import OrderedDict
 import logging
-
-log = logging.getLogger('multiqc.modules.strainline')
-log.critical('Properly imported!')
+import os
+import sys
 
 from multiqc import config
 from multiqc.base_module import BaseMultiqcModule # type: ignore
 from multiqc.plots import linegraph, bargraph # type: ignore
 from multiqc.plots.table_object import ColumnMeta # type: ignore
+from multiqc.base_module import ModuleNoSamplesFound # type: ignore
 
-import os
 import yaml # type: ignore
-from collections import OrderedDict
-import sys
 
+log = logging.getLogger('damlabqc.strainline')
 
 class MultiqcModule(BaseMultiqcModule):
     def __init__(self):
@@ -24,72 +23,71 @@ class MultiqcModule(BaseMultiqcModule):
             info='is a pipeline for haplotype reconstruction of viral sequences.'
         )
 
-        # Add to module config
-        headers = {
-            'haplotype_count': ColumnMeta(
-                rid='1', 
-                title='Haplotype Count',
-                description='Number of haplotypes reconstructed',
-                scale='RdYlGn',
-                format='{:,.0f}',
-                placement=900.0  # Place towards left side of table
-            ),
-            'haplotype_freqs_max': ColumnMeta(
-                rid='2',
-                title='Max Frequency',
-                description='Maximum frequency of any haplotype',
-                scale='RdYlGn',
-                min=0,
-                max=1,
-                format='{:,.3f}',
-                placement=910.0
-            )
-        }
+        # Initialize data storage
+        self._strainline_data = dict()
         
-        #config.module_order = ["strainline"] + config.module_order
-        #config.general_stats_headers['strainline'] = headers
-
-        # Find all strainline output files
-        self.strainline_data = dict()
+        self._collect_log_files()
+        self._add_general_stats()
+        self._add_sections()
+    
+    def _collect_log_files(self):
+        """Collect data from log files found my multiqc."""
         
-        log.critical(f"Checking for strainline log files")
-        # Look for strainline log files
         for f in self.find_log_files('strainline'):
-            log.critical(f"Found strainline log file: {f['fn']}")
-            
-            self.parse_strainline_logs(f)
-            
-        # Filter samples if we have any
-        self.strainline_data = self.ignore_samples(self.strainline_data)
-        
-        # No samples found
-        if len(self.strainline_data) == 0:
-            raise UserWarning
-            
-        log.info(f"Found {len(self.strainline_data)} reports")
-        
-        # Write parsed report data to a file
-        self.write_data_file(self.strainline_data, 'multiqc_strainline')
+            self._parse_strainline_log(f)
 
-        # Add to general stats table
+        # Filter out ignored samples if we have any using multiqc public method
+        self._strainline_data = self.ignore_samples(self._strainline_data)
+
+        # No samples found
+        if len(self._strainline_data) == 0:
+            raise ModuleNoSamplesFound
+        
+        log.info(f"Found {len(self._strainline_data)} reports")
+        
+        # Write parsed report data to a file using multiqc public method
+        self.write_data_file(self._strainline_data, 'multiqc_strainline')
+    
+    
+    
+    def _add_general_stats(self):
+        """Add to the general stats table at the top of the report."""
+
+        # Create general stats data from parsed strainline data
         general_stats_data = {}
-        for s_name, metrics in self.strainline_data.items():
+        for s_name, metrics in self._strainline_data.items():
             general_stats_data[s_name] = {
                 'haplotype_count': metrics['haplotype_count'],
                 'haplotype_freqs_max': metrics['haplotype_freqs']['max']
             }
-        
-        # TODO: Add headers to general stats table
-        #self.general_stats_addcols(general_stats_data, headers)
 
-        # Adding without headers for now
-        self.general_stats_addcols(general_stats_data) #headers
+        # Make nice header columns
+        headers = {
+            'haplotype_count': {
+                'title':'Haplotype Count',
+                'description':'Number of haplotypes reconstructed',
+                'scale':'RdYlGn',
+                'format':'{:,.0f}',
+                'placement':900.0  # Place towards left side of table
+            },
+            'haplotype_freqs_max': {
+                'title':'Max Frequency',
+                'description':'Maximum frequency of any haplotype',
+                'scale':'RdYlGn',
+                'min':0,
+                'max':1,
+                'format': '{:,.1%}',
+                'placement':910.0
+            }
+        }
         
-        # Add sections to report
-        #self.add_sections()
+        # Add to general stats table using multiqc public method
+        self.general_stats_addcols(general_stats_data, headers)
         
-    def parse_strainline_logs(self, f):
-        """Parse strainline log files."""
+
+    def _parse_strainline_log(self, f):
+        """Parse single strainline log file."""
+
         try:
             # Load YAML data
             data = yaml.safe_load(f['f'])
@@ -98,7 +96,7 @@ class MultiqcModule(BaseMultiqcModule):
                 return
                 
             # Get sample name from the data
-            s_name = data.get('sample_id', f['s_name'])
+            s_name = data.get('sample_name', f['fn'])
             s_name = self.clean_s_name(s_name, f)
             
             # Parse metrics
@@ -116,71 +114,85 @@ class MultiqcModule(BaseMultiqcModule):
                 }
             }
             
-            self.strainline_data[s_name] = metrics
+            self._strainline_data[s_name] = metrics
             
         except Exception as e:
-            log.debug(f"Error parsing file {f['fn']}: {e}")
-            return
+            log.warning(f"Error parsing file {f['fn']}: {e}")
+            raise e
         
-    def add_sections(self):
+    def _add_sections(self):
         """Add sections to MultiQC report."""
-        # Add haplotype count barplot
-
-        # TODO: Add haplotype count barplot correctly
+        
+        self._add_haplotype_counts()
+        self._add_haplotype_lengths()
+        self._add_haplotype_freqs()
+        
+    def _add_haplotype_counts(self):
+        """Add haplotype count barplot to report."""
+        
+        count_data = {}
+        for s_name, d in self._strainline_data.items():
+            count_data[s_name] = {'haplotype_count': d['haplotype_count']}
 
         self.add_section(
             name='Haplotype Counts',
             anchor='strainline-haplotype-counts',
             description='Number of haplotypes reconstructed per sample',
-            plot=bargraph.plot({
-                'id': 'strainline_haplotype_counts',
-                'data': {s_name: {'haplotype_count': d['haplotype_count']}
-                        for s_name, d in self.strainline_data.items()},
-                'title': 'Strainline: Haplotype Counts',
-                'ylab': 'Number of Haplotypes'
-            })
+            plot=bargraph.plot(count_data,
+                               pconfig = {
+                                   'id': 'strainline_haplotype_counts',
+                                   'title': 'Strainline: Haplotype Counts',
+                                   'ylab': 'Number of Haplotypes'
+                               }
+            )
         )
+    
+    def _add_haplotype_lengths(self):
+        """Add haplotype length distribution to report."""
         
-        # Add haplotype length distribution
         length_data = {
             s_name: {
                 'Maximum': d['haplotype_lengths']['max'],
                 'Mean': d['haplotype_lengths']['mean'],
                 'Minimum': d['haplotype_lengths']['min']
             }
-            for s_name, d in self.strainline_data.items()
+            for s_name, d in self._strainline_data.items()
         }
         
         self.add_section(
             name='Haplotype Lengths',
             anchor='strainline-haplotype-lengths',
             description='Distribution of haplotype lengths per sample',
-            plot=bargraph.plot({
-                'id': 'strainline_haplotype_lengths',
-                'data': length_data,
-                'title': 'Strainline: Haplotype Lengths',
-                'ylab': 'Length (bp)'
-            })
+            plot=bargraph.plot(length_data,
+                               pconfig = {
+                                   'id': 'strainline_haplotype_lengths',
+                                   'title': 'Strainline: Haplotype Lengths',
+                                   'ylab': 'Length (bp)'
+                               }
+            )
         )
+
+    def _add_haplotype_freqs(self):
+        """Add haplotype frequency distribution to report."""
         
-        # Add haplotype frequency distribution
         freq_data = {
             s_name: {
                 'Maximum': d['haplotype_freqs']['max'],
                 'Mean': d['haplotype_freqs']['mean'],
                 'Minimum': d['haplotype_freqs']['min']
             }
-            for s_name, d in self.strainline_data.items()
+            for s_name, d in self._strainline_data.items()
         }
         
         self.add_section(
             name='Haplotype Frequencies',
             anchor='strainline-haplotype-freqs',
             description='Distribution of haplotype frequencies per sample',
-            plot=bargraph.plot({
-                'id': 'strainline_haplotype_freqs',
-                'data': freq_data,
-                'title': 'Strainline: Haplotype Frequencies',
-                'ylab': 'Frequency'
-            })
+            plot=bargraph.plot(freq_data,
+                               pconfig = {
+                                   'id': 'strainline_haplotype_freqs',
+                                   'title': 'Strainline: Haplotype Frequencies',
+                                   'ylab': 'Frequency'
+                               }
+            )
         ) 
